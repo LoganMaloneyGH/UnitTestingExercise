f0c69e9cb508fd8cc7bc991e3a6dbfffa76376d7
logan Maloney <shakenasty13@gmail.com>
2021-12-16 20:12:50 -0600

    initial commit

--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -9,18 +9,40 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
-
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3; 
+        }
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
-
+            public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;  
+        }
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;  
+        }
         // Create a Divide method that passes 2 integers
 
+        public double Divide(int num1, int num2)
+        {
+            return num1 / num2; 
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public bool IsCharSymbol(char testChar)
+        {
+            return char.IsSymbol(testChar); 
 
+        }
+
+        public bool IscharSymbol(char testChar2)
+        {
+            return char.IsLetterOrDigit(testChar2);
+        }
         
     }
 }
